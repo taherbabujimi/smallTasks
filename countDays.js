cd657aaf30ee3c4a21f9fba1498cd2d7e71730cd
taherbabujimi <taher.babuji@mindinventory.com>
2024-11-05 13:59:15 +0530

    Update countDays.js, now it will only skip dates which are not already skipped

--- countDays.js
+++ countDays.js
@@ -1,11 +1,11 @@
 const startDateDay = 1;
-const startDateMonth = 7;
+const startDateMonth = 9;
 const startDateYear = 2020;
-const startDateDayName = "Mon";
+const startDateDayName = "Tue";
 
-const finishDateDay = 22;
-const finishDateMonth = 9;
-const finishDateYear = 2023;
+const finishDateDay = 29;
+const finishDateMonth = 1;
+const finishDateYear = 2030;
 
 let totalDays = 0;
 let daysInWeek = 7;
@@ -13,9 +13,11 @@ let daysInWeek = 7;
 const weekdays = ["Mon", "Tue", "Wed", "Thu", "Fri", "Sat", "Sun"];
 let weekIndex = weekdays.indexOf(startDateDayName);
 
-let weekCounter = 0;
+let weekCounter = 1;
 
-let skipDates = ["22/8/2024", "5/4/2024", "16/8/2024"];
+let skipDates = ["12/9/2020", "13/9/2020", "10/10/2020", "11/10/2020"];
+
+let skippedDatesMap = new Set();
 
 if (startDateYear === finishDateYear && startDateMonth === finishDateMonth) {
   totalDays = finishDateDay - startDateDay;
@@ -64,33 +66,69 @@ if (startDateYear === finishDateYear && startDateMonth === finishDateMonth) {
       }
 
       if (i === startDateYear && j === startDateMonth) {
-        daysInMonth = daysInMonth - startDateDay;
+        daysInMonth = daysInMonth - startDateDay + 1;
       } else if (i === finishDateYear && j === finishDateMonth) {
         daysInMonth = finishDateDay;
       }
 
-      console.log("days in month: ", daysInMonth);
-
       let fullWeeks = Math.floor(daysInMonth / 7);
-      console.log("FullWeeks: ", fullWeeks);
       let extraDays = daysInMonth % 7;
-      console.log("Extra days: ", extraDays);
 
       for (let k = 0; k < fullWeeks; k++) {
-        totalDays += daysInWeek;
-        // if (weekCounter % 2 === 0) {
-        //   totalDays += 5;
-        // } else {
-        //   totalDays += daysInWeek;
-        // }
-        // weekCounter++;
+        if (weekCounter % 2 === 0) {
+          totalDays += 5;
+
+          let saturdayDate = `${6 + 7 * k - weekIndex}/${j}/${i}`;
+          let sundayDate = `${7 + 7 * k - weekIndex}/${j}/${i}`;
+
+          skippedDatesMap.add(saturdayDate);
+          skippedDatesMap.add(sundayDate);
+        } else {
+          totalDays += daysInWeek;
+        }
+        weekCounter++;
+        weekIndex = (weekIndex + 7) % 7;
       }
 
       if (extraDays > 0) {
-        totalDays += weekCounter % 2 === 0 ? Math.min(extraDays, 5) : extraDays;
+        for (let d = 1; d <= extraDays; d++) {
+          if (weekCounter % 2 === 0) {
+            if (d === 6) {
+              skippedDatesMap.add(`${d}/${j}/${i}`);
+            } else if (d === 7) {
+              skippedDatesMap.add(`${d}/${j}/${i}`);
+            }
+            totalDays += Math.min(extraDays - d + 1, 5);
+            break;
+          } else {
+            totalDays++;
+          }
+          weekCounter++;
+          weekIndex = (weekIndex + 1) % 7;
+        }
       }
-      console.log("min:", Math.min(extraDays, 5));
+
       weekCounter++;
+
+      for (let l = 0; l < skipDates.length; l++) {
+        let skipDate = skipDates[l].split("/");
+        let skipDay = parseInt(skipDate[0]);
+        let skipMonth = parseInt(skipDate[1]);
+        let skipYear = parseInt(skipDate[2]);
+
+        let skipDateIdentifier = `${skipDay}/${skipMonth}/${skipYear}`;
+
+        if (
+          skipYear === i &&
+          skipMonth === j &&
+          skipDay >= 1 &&
+          skipDay <= daysInMonth &&
+          !skippedDatesMap.has(skipDateIdentifier)
+        ) {
+          totalDays -= 1;
+          skippedDatesMap.add(skipDateIdentifier);
+        }
+      }
     }
   }
 }
