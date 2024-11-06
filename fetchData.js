1c74fc8d4104108eaed7f84d638ac5eb5bf5d8fd
taherbabujimi <taher.babuji@mindinventory.com>
2024-11-06 15:22:11 +0530

    Update fetchData.js, created two separate functions.

--- fetchData.js
+++ fetchData.js
@@ -7,6 +7,7 @@ let data = [
   {
     id: 2,
     name: "fv",
+    dependecies: [3],
     status: 1,
   },
   {
@@ -23,36 +24,69 @@ let data = [
   },
 ];
 
-for (let i = 0; i < data.length; i++) {
-  if (data[i].status === 1 && data[i].dependecies === undefined) {
-    console.log(data[i].name);
-  } else if (data[i].status === 1 && data[i].dependecies !== undefined) {
-    let status = false;
-    for (let j = 0; j < data.length; j++) {
-      if (data[j].dependecies !== undefined) {
-        if (
-          data[i].dependecies.includes(data[j].id) &&
-          data[j].dependecies.includes(data[i].id)
-        ) {
-          console.log(
-            `Circular dependencies detected between: ${data[i].id} and ${data[j].id}`
-          );
+function fetchData(id) {
+  let dataFound = false;
+  for (let i = 0; i < data.length; i++) {
+    if (data[i].id === id) {
+      if (data[i].status === 1 && data[i].dependecies === undefined) {
+        console.log(data[i].name);
+      } else if (data[i].status === 1 && data[i].dependecies !== undefined) {
+        let status = false;
+        for (let j = 0; j < data.length; j++) {
+          if (data[i].dependecies.includes(data[j].id)) {
+            if (data[j].status === 1) {
+              status = true;
+            } else {
+              status = false;
+            }
+          }
         }
-      }
 
-      if (data[i].dependecies.includes(data[j].id)) {
-        if (data[j].status === 1) {
-          status = true;
+        if (status === true) {
+          console.log(data[i].name);
         } else {
-          status = false;
+          console.log(data[i].name, "'s dependecies have status 0");
         }
       }
-    }
-
-    if (status === true) {
-      console.log(data[i].name);
-    } else {
-      console.log(data[i].name, "'s dependencies have status 0");
+      dataFound = true;
+      break;
     }
   }
+
+  if (dataFound === false) {
+    console.log("Data with this id not exists.");
+  }
 }
+
+function checkCirculardependecies(id, visited = new Set()) {
+  let dataFound = false;
+
+  for (let i = 0; i < data.length; i++) {
+    if (data[i].id === id) {
+      if (data[i].dependecies !== undefined) {
+        if (visited.has(id)) {
+          console.log(`Circular dependencies detected at: ${id}`);
+          return;
+        } else {
+          console.log(`Does Not found any Circular dependencies.`);
+        }
+
+        visited.add(id);
+
+        for (let j = 0; j < data[i].dependecies.length; j++) {
+          let depId = data[i].dependecies[j];
+          checkCirculardependecies(depId, visited);
+        }
+      }
+      dataFound = true;
+      break;
+    }
+  }
+
+  if (dataFound === false) {
+    console.log("Data with this id does not exist.");
+  }
+}
+
+fetchData(3);
+checkCirculardependecies(4);
